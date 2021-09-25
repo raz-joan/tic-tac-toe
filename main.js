eb308d150644aed4c0326d0e31d5822b50de0c28
Joan Rasmussen <raz.joan@gmail.com>
2021-09-24 20:51:52 -0600

    Add pseudocode for main.js DOM and classes interactions

---
+++ main.js
@@ -0,0 +1,26 @@
+// query each players' score box in their aside
+// query player's turn box
+// query winner box
+// query the 9 boxes (zero through eight)
+
+// listeners on the 9 boxes (zero through eight)
+
+// when a box is clicked ...
+  // does it contain an img either wheel or wheat?
+  // if yes, do nothing
+  // if no, innerHTML the clicked box with the current player's token
+  // in this case `element.children.length = 0` i think?
+
+// keep track of turn with modulo for even/odd
+// how to switch first player from game to game?
+// how to switch innerText for each new turn?
+
+// how to check for win?
+// how to update the scores in instances? and then, on the DOM?
+// how to display the winner with innerText?
+
+// setTimeout for 3 - 5 delay before board resets on screen?
+// how to reset board?
+
+// how to save each player's win total to localStorage?
+// how to retrieve each player's win total from localStorage and set innerText?
